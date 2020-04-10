0e5588cd9986e0f2f7a033dba312f3190cfb1f39
lorenanicole <me@lorenamesa.com>
2020-04-09 23:55:04 -0500

    set site url  to use https

--- pelicanconf.py
+++ pelicanconf.py
@@ -5,7 +5,7 @@ from __future__ import unicode_literals
 AUTHOR = 'PyLadies'
 SITENAME = 'PyLadies Global Council'
 SITEDESCRIPTION = 'Learn more about the PyLadies Global Council.'
-SITEURL = 'http://pyladies.github.io'
+SITEURL = 'https://pyladies.github.io'
 
 # plugins
 PLUGIN_PATHS = ['../pelican-plugins']
