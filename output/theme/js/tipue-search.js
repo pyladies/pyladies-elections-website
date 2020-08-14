9d14026485ff8d3ea2315c6a0f6cbfca44d9e62d
Sara EL-ATEIF <sara.elateif@gmail.com>
2020-08-14 20:06:14 +0100

    Remove output/ folder

--- output/theme/js/tipue-search.js
+++
@@ -1,8 +0,0 @@
-jQuery(document).ready(function() {
-     var siteUrl = jQuery('#tipue_search_input').data('siteurl');
-     jQuery('#tipue_search_input').tipuesearch({
-         'show': 10,
-         'mode': 'json',
-         'contentLocation': siteUrl + '/tipuesearch_content.json'
-     });
-});
